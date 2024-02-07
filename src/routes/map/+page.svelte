5e97b4121e645973fe96362531db07078df5de67
vincent <vincentcharlebois@gmail.com>
2024-02-07 16:47:05 -0500

    Add title and update marker popup content

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -7,15 +7,62 @@
 	import { markersStore, initialViewStore } from '$lib/stores';
 
 </script>
-
+<h1>
+    counter*map
+</h1>
 <div class="w-full h-screen">
-    <Leaflet view={$initialViewStore} zoom={12}>
-        {#each $markersStore as { latLng, visible, popupContent }, index (latLng.join(',') + '-' + index)}
+
+    <Leaflet view={$initialViewStore} zoom={4}>
+        {#each $markersStore as { latLng, visible, name, description, photos }, index (latLng.join(',') + '-' + index)}
             {#if visible}
                 <Marker {latLng} width={20} height={20}>
-                    <Popup>{popupContent}</Popup>
+                    <Popup>
+                        <h3>
+                            {name}
+                        </h3>
+
+                        {#if photos && photos.length}
+                        <div class='flex flex-row overflow-x-auto'>
+                                {#each photos as {url, alt}}
+                                    <img src={`https://www.veterans.gc.ca/images/remembrance/memorials/national-inventory-canadian-memorials/mem/${url}`} alt="{alt}">
+                                {/each}
+                            </div>
+                        {/if}
+
+                        {#if description}
+                            <p class="description max-h-32 overflow-y-auto">        
+                                {@html description}
+                            </p>
+                        {/if}
+
+                    </Popup>
                 </Marker>
             {/if}
         {/each}
     </Leaflet>
 </div>
+
+<style>
+    h1 {
+        font-family: 'Itim', sans-serif;
+        font-size: 3rem;
+        position: fixed;
+        top: 1%;
+        left: 50%; 
+        transform: translateX(-50%);
+        text-align: center;
+        z-index: 9998;
+        pointer-events: none; 
+    }
+
+    img {
+        width: auto;
+        height: 150px;
+        object-fit: contain;
+        margin: 5px;
+    }
+
+    p.description {
+        margin-top: 2px;
+    }
+</style>
