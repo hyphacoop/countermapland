04f768d921d0f12acb123d95586bbbec922e369b
vincent <vincentcharlebois@gmail.com>
2024-02-15 17:43:55 -0500

    Update base path in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
         paths: {
-            base: '/countermapland',
+            base: process.env.NODE_ENV === 'staging' ? '/countermapland' : '',
         }
 	}
 };
