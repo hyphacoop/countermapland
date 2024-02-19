217a89e0b5be548c66c8eac6e7c33a5cb4877b51
vincent <vincentcharlebois@gmail.com>
2024-02-19 10:56:38 -0500

    Fix base URL in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
         paths: {
-            base: process.env.NODE_ENV === 'staging' ? 'https://hyphacoop.github.io/countermapland/' : '',
+            base: process.env.NODE_ENV === 'staging' ? 'https://hyphacoop.github.io/countermapland' : '',
         }
 	}
 };
