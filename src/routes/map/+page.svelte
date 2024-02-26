01859295ff1244851616d7a9a02993f394193f68
vincent <vincentcharlebois@gmail.com>
2024-02-26 09:57:17 -0500

    Enhance image display logic for varied counts and refine styles
    - Implement conditional logic for 1, 2, and 3+ image scenarios in map popups
    - Introduce centered-left and centered-right classes for better alignment of two images
    - Update CSS for consistent image presentation and spacing
    - Adjust base URL application for image sources

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,94 +1,189 @@
 <script>
-    import { derived } from 'svelte/store';
-    import { onMount } from 'svelte';
-    import L from 'leaflet';
+  import { derived } from "svelte/store";
+  import { onMount } from "svelte";
+  import L from "leaflet";
 
-	import '../../app.css';
-	import Leaflet from '$lib/Map/Leaflet.svelte';
-	import Marker from '$lib/Map/Marker.svelte';
-	import Popup from '$lib/Map/Popup.svelte';
+  import "../../app.css";
+  import Leaflet from "$lib/Map/Leaflet.svelte";
+  import Marker from "$lib/Map/Marker.svelte";
+  import Popup from "$lib/Map/Popup.svelte";
 
-    import Search from '$lib/UI/Search.svelte';
+  import Search from "$lib/UI/Search.svelte";
 
-	import { markersStore, initialViewStore, currentViewStore, mapBoundsStore, darkMode  } from '$lib/stores';
+  import {
+    markersStore,
+    initialViewStore,
+    currentViewStore,
+    mapBoundsStore,
+    darkMode,
+  } from "$lib/stores";
 
-    const visibleMarkers = derived([markersStore, mapBoundsStore], ([$markersStore, $mapBoundsStore]) => {
-        if (!$mapBoundsStore || typeof $mapBoundsStore.contains !== 'function') {
-            return []; // Return an empty array if bounds are undefined or invalid
-        }
-        return $markersStore.filter(marker => {
-            // Convert the array to a LatLng object
-            const [lat, lng] = marker.latLng;
-            return $mapBoundsStore.contains(L.latLng(lat, lng));
-        });
-    });
+  let baseUrl =
+    "https://www.veterans.gc.ca/images/remembrance/memorials/national-inventory-canadian-memorials/mem/";
 
-    function handleUpdateView(event) {
-        const { latLng } = event.detail;
-        currentViewStore.set({ lat: latLng[0], lng: latLng[1] });
+  const visibleMarkers = derived(
+    [markersStore, mapBoundsStore],
+    ([$markersStore, $mapBoundsStore]) => {
+      if (!$mapBoundsStore || typeof $mapBoundsStore.contains !== "function") {
+        return []; // Return an empty array if bounds are undefined or invalid
+      }
+      return $markersStore.filter((marker) => {
+        // Convert the array to a LatLng object
+        const [lat, lng] = marker.latLng;
+        return $mapBoundsStore.contains(L.latLng(lat, lng));
+      });
     }
-    
+  );
+
+  function handleUpdateView(event) {
+    const { latLng } = event.detail;
+    currentViewStore.set({ lat: latLng[0], lng: latLng[1] });
+  }
 </script>
-<h1 class={$darkMode === 'dark' ? 'darkmode' : ''}>
-    *countermap
-</h1>
+
+<h1 class={$darkMode === "dark" ? "darkmode" : ""}>*countermap</h1>
 <div class="w-full h-screen">
-    <Search on:updateView="{handleUpdateView}" />
-    <Leaflet view={$currentViewStore} zoom={8}>
-        {#each $visibleMarkers as { latLng, visible, name, description, photos }, index (latLng.join(',') + '-' + index)}
-            {#if visible}
-                <Marker {latLng} width={20} height={20}>
-                    <Popup>
-                        <h3>
-                            {name}
-                        </h3>
-
-                        {#if photos && photos.length}
-                        <div class='flex flex-row overflow-x-auto'>
-                                {#each photos as {url, alt}}
-                                    <img src={`https://www.veterans.gc.ca/images/remembrance/memorials/national-inventory-canadian-memorials/mem/${url}`} alt="{alt}">
-                                {/each}
-                            </div>
-                        {/if}
-
-                        {#if description}
-                            <p class="description max-h-32 overflow-y-auto">        
-                                {@html description}
-                            </p>
-                        {/if}
-
-                    </Popup>
-                </Marker>
+  <Search on:updateView={handleUpdateView} />
+  <Leaflet view={$currentViewStore} zoom={8}>
+    {#each $visibleMarkers as { latLng, visible, name, description, photos }, index (latLng.join(",") + "-" + index)}
+      {#if visible}
+        <Marker {latLng} width={20} height={20}>
+          <Popup>
+            {#if photos && photos.length}
+              <div class="image-container">
+                {#if photos && photos.length === 1}
+                  <img
+                    src={`${baseUrl}${photos[0].url}`}
+                    alt={photos[0].alt}
+                    class="main single"
+                  />
+                {:else if photos.length === 2}
+                  <img
+                    src={`${baseUrl}${photos[0].url}`}
+                    alt={photos[0].alt}
+                    class="centered-left"
+                  />
+                  <img
+                    src={`${baseUrl}${photos[1].url}`}
+                    alt={photos[1].alt}
+                    class="centered-right"
+                  />
+                {:else if photos.length >= 3}
+                  <img
+                    src={`${baseUrl}${photos[1].url}`}
+                    alt={photos[1].alt}
+                    class="side left"
+                  />
+                  <img
+                    src={`${baseUrl}${photos[0].url}`}
+                    alt={photos[0].alt}
+                    class="main"
+                  />
+                  <img
+                    src={`${baseUrl}${photos[2].url}`}
+                    alt={photos[2].alt}
+                    class="side right"
+                  />
+                {/if}
+              </div>
             {/if}
-        {/each}
-    </Leaflet>
+            <h2>
+              {name}
+            </h2>
+            {#if description}
+              <p class="description max-h-32 overflow-y-auto">
+                {@html description}
+              </p>
+            {/if}
+          </Popup>
+        </Marker>
+      {/if}
+    {/each}
+  </Leaflet>
 </div>
 
 <style>
-    h1 {
-        font-family: 'Itim', sans-serif;
-        font-size: 3rem;
-        position: fixed;
-        top: 1%;
-        left: 50%; 
-        transform: translateX(-50%);
-        text-align: center;
-        z-index: 9998;
-        pointer-events: none; 
-    }
+  h1 {
+    font-family: "Itim", sans-serif;
+    font-size: 3rem;
+    position: fixed;
+    top: 1%;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+    z-index: 9998;
+    pointer-events: none;
+  }
 
-    .darkmode {
-        color: white;
-    }
+  h2 {
+    margin-top: 1rem;
+    color: #000;
+    font-family: Itim;
+    font-size: 2.0625rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
 
-    img {
-        width: auto;
-        height: 150px;
-        object-fit: contain;
-        margin: 5px;
-    }
+  .darkmode {
+    color: white;
+  }
 
-    p.description {
-        margin-top: 2px;
-    }
+  img {
+    width: auto;
+    height: 100px;
+    object-fit: contain;
+    margin: 5px;
+    border: 0.075rem solid #000;
+  }
+
+  .image-container {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    perspective: 100rem;
+    margin: 1.25rem 0rem;
+    height: 100px;
+  }
+
+  .image-container img.main {
+    z-index: 2;
+    transition: transform 0.3s ease;
+    position: relative;
+  }
+
+  .image-container img.side {
+    height: 90px;
+    position: absolute;
+    transition: transform 0.3s ease;
+    z-index: 1;
+  }
+
+  .image-container img.left {
+    left: 0%;
+    top: 15%;
+    transform: rotateZ(5deg);
+  }
+
+  .image-container img.right {
+    right: 0%;
+    top: 15%;
+    transform: rotateZ(-5deg);
+  }
+
+  .image-container img.centered-left {
+    transform: translateX(10%) rotateZ(5deg); /* Adjust the percentage as needed */
+    z-index: 1;
+  }
+
+  .image-container img.centered-right {
+    transform: translateX(-10%) rotateZ(-5deg); /* Adjust the percentage as needed */
+    z-index: 1;
+  }
+
+  p.description {
+    margin-top: 2px;
+  }
 </style>
