eedbf2e2d77598dc39fb84e6ead8286b549eace9
vincent <vincentcharlebois@gmail.com>
2024-02-27 17:14:48 -0500

    Add About page content and styles

---
+++ src/lib/Pages/About.svelte
@@ -0,0 +1,61 @@
+<p>
+    
+This project was initiated on territory occupied by many nations including the Mississaugas of the Credit, the Anishnabeg, the Chippewa, the Haudenosaunee and the Wendat peoples for thousands of years. Today Tkaronto is home to many diverse Indigenous people from across Turtle Island. We recognize that although this land is covered by Treaty 13, colonialism is an ongoing process. 
+
+</p>
+
+<p>
+
+    Beyond this land acknowledgement, we are committed to continually questioning how countermap can work with Indigenous communities to expose, impair, and end settler colonialism.
+
+
+</p>
+
+
+<ol>
+
+
+  <li>  Building public datasets around the current monument landscape and its morphology; </li>
+  <li>    Disentangling the processes, agencies, policies, and legal frameworks that manage and maintain the current monument landscape; </li>
+    <li>   Investigating the existence and emergence of racist and colonial spaces and institutions in Canada, including spaces and institutions that no longer exist;  </li>
+        <li>   Recognizing a broader definition of ‘monument’ that includes living or lost ecologies, communities, histories, and movements;  </li>
+            <li>  Continually challenging the imperial roots of mapping technology and its projections; </li>
+                <li>  Countering histories of erasure and inequality through new discussions, interventions, collaborations, and a culture of openness/learning;  </li>
+                    <li>      Supporting and building infrastructure in arts and culture that expand practice beyond prevalent colonial frameworks.  </li>
+    
+
+
+</ol>
+
+<p>
+    countermap.land is in progress. Although our project is currently limited to Canada’s settler colonial borders, we are building capacity to expand the project. We are also interested in projects that question cartography’s colonial roots. Interested in collaborating? Discussing? Sharing? Contact us. 
+</p>
+
+<ul>
+    <li>Evelyn Hoffman </li>
+    <li>   Heather Breeze </li>
+    <li>   Jamie Lipson </li>
+    <li> Kearon Roy Taylor </li>
+    <li>  Kevin Henriques </li>
+    <li>  Michelle Creeley </li>
+    <li>   Monica Hutton </li>
+    <li>  Nicole Kelly Westman</li> 
+    <li> Tara Marchelewicz </li>
+    <li> Victor Temperano</li>
+    <li> Syrus Marcus Ware </li>
+    <li>    Christina Battle</li>
+    
+
+</ul>
+
+<style>
+    p {
+        margin-bottom: 1.5rem;
+    }
+    ol {
+        margin-bottom: 1.5rem;
+    }
+    ul {
+        margin-bottom: 1.5rem;
+    }
+</style>
