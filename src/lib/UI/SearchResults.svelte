596e2ee55d8721ff7e81ac271909cf8692965b2e
vincent <vincentcharlebois@gmail.com>
2024-02-29 09:16:08 -0500

    Refactor SearchResults.svelte component

--- src/lib/UI/SearchResults.svelte
+++ src/lib/UI/SearchResults.svelte
@@ -1,47 +1,71 @@
-
 <script>
-    import { fly } from "svelte/transition";
-    import { createEventDispatcher } from 'svelte';
+  import { fly } from "svelte/transition";
+  import { createEventDispatcher } from "svelte";
 
-    const dispatch = createEventDispatcher();
+  import { currentSidebar } from "$lib/stores";
 
-    import SearchItem from "./SearchItem.svelte";
+  import closeImage from "$lib/icons/close.png";
 
-    export let results = {};
-    export let baseUrl = "";
+  const dispatch = createEventDispatcher();
 
-    $: console.log('results', results);
+  import SearchItem from "./SearchItem.svelte";
 
-    function handleDetails(event) {
-        dispatch('closeAndShowDetails', event.detail);
-    }
+  export let results = {};
+  export let baseUrl = "";
+
+  $: console.log("results", results);
+
+  function handleDetails(event) {
+    dispatch("closeAndShowDetails", { id: event.detail.id, event: event });
+  }
+
+  function closeSearch() {
+    currentSidebar.set(null);
+  }
 </script>
 
-<div class="sidebar"
-in:fly={{ x: 300, duration: 800 }}
-out:fly={{ x: 300, duration: 800 }}
+<div
+  class="sidebar"
+  in:fly={{ x: 300, duration: 800 }}
+  out:fly={{ x: 300, duration: 800 }}
 >
-<p>{results.length}
-{results.length > 1 ? 'places ' : 'place '}
-replied to your call...
-</p>
-{#each results as { name, photos, municipality, id }, index (id + "-" + index)}
-    <SearchItem {name} {photos} {municipality} {id} {baseUrl} on:closeAndShowDetails={handleDetails}/>
-{/each}
+  <button class="close-button" on:click={closeSearch}>
+    <img src={closeImage} alt="Close Sidebar" />
+  </button>
+  <p>
+    {results.length}
+    {results.length > 1 ? "places " : "place "}
+    replied to your call...
+  </p>
+  {#each results as { name, photos, municipality, id }, index (id + "-" + index)}
+    <SearchItem
+      {name}
+      {photos}
+      {municipality}
+      {id}
+      {baseUrl}
+      on:closeAndShowDetails={(event) => handleDetails(event)}
+    />
+  {/each}
 </div>
 
 <style>
-    .sidebar {
-        background: #FFD966;
-        width: 550px;
-        height: 100%;
-        position: fixed;
-        top: 0;
-        right: 0;
-        box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5);
-        overflow-y: auto;
-        z-index: 9999;
-        padding: 0 1.25rem;
-        padding-top: 2.69rem;
-    }
+  .sidebar {
+    background: #ffd966;
+    width: 550px;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    right: 0;
+    box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5);
+    overflow-y: auto;
+    z-index: 9999;
+    padding: 0 1.25rem;
+    padding-top: 2.69rem;
+  }
+  .close-button {
+    position: absolute;
+    top: 1.19rem;
+    right: 1.19rem;
+  }
 </style>
