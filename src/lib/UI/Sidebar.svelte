07c09f475dce72a12cf9ec2efb565a3781e17617
vincent <vincentcharlebois@gmail.com>
2024-03-06 10:00:13 -0500

    Add disclaimer for monuments

--- src/lib/UI/Sidebar.svelte
+++ src/lib/UI/Sidebar.svelte
@@ -193,11 +193,13 @@
     </div>
   </div>
   {/if}
+  {#if $selectedMarker.challengesPower === false}
   <p class='disclaimer'>
     <i>
       This information is imported from third-party datasets and does not reflect the opinions of countermap.
     </i>
   </p>
+  {/if}
 </div>
 
 <style>
