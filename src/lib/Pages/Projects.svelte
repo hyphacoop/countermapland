57995a06b56d6ccbdbfbdc8000f79058083545f9
vincent <vincentcharlebois@gmail.com>
2024-04-02 10:09:43 -0400

    Update copy

--- src/lib/Pages/Projects.svelte
+++ src/lib/Pages/Projects.svelte
@@ -8,8 +8,7 @@
 </script>
 
 <p>
-  countermap works through collaborations and commissions to build histories and
-  alternative presents/futures for the land.
+  *countermap works through collaborations and commissions to understand history and imagine alternative futures. 
 </p>
 
 <p>
@@ -17,28 +16,46 @@
 </p>
 
 <p>
-  Between 2021-23, countermap.land distributed a series of micro-grants to
-  artist-run centres and collectives across Canada to conduct a series of
-  workshops. Workshop facilitators and participants were asked to use and
-  critique a beta-test version of countermap.land. Varying in scale, approach,
-  and pedagogy, these workshops and the resulting feedback shaped the current
-  iteration of the project.
+  Between 2021-23, *countermap distributed a series of micro-grants to artist-run centres and collectives across <strike>Canada</strike> to conduct a series of workshops. Workshop facilitators and participants were asked to use and critique a beta-test version of countermap.land.  Varying in scale, approach, and pedagogy, these workshops and the resulting feedback shaped the current iteration of the project.
 </p>
+
+<hr>
 
 <img src={project1} alt="swamp" />
 
+
+<p>
 <a href="https://reflexurbanism.wordpress.com/">
   <strong> reflex urbanism </strong>
 </a>
-
+</p>
 <p>
+  <i>
   “I think a big way of countering the settler-colonial condition is to give
   space for open-endedness, dreaming, and feeling. Bring these in conversation
   with a focus on Indigenous, Black and newcomer populations would give lots of
   space to not only critique the settler-colony, but also imagine (in the most
   active use of this word) alternatives. And more partnership-activations like
   this workshop will help keep the movement alive and growing!”
+</i>
 </p>
+
+<p>
+  Reflex Urbanism is an urban practice studio transforming the culture of cities. Reflex Urbanism understand, from experience, the complex nature of contemporary urban life. The same places
+marginalized people flock to marginalize us in return. Rather than call to centre the
+marginalized, Reflex uplifts power among the margins. Through relational research, planning,
+training, and action, Reflex addresses urban issues by and for equity-deserving communities.
+</p>
+
+<p>
+  Reflex is based in the Haudenosaunee and Anishinaabe territories known as Tkaronto, Ogimaa
+Mikana, and Toronto. As such, we are beholden to the Dish With One Spoon and Two Row
+Wampum covenants. Our studio serves those for whom these covenants also apply.
+</p>
+
+<p>Learn more <a href="https://reflexurbanism.wordpress.com/">here</a> and <a href='https://darianrazdar.info/Poetics-of-Place'>here</a>.</p> 
+
+<hr>
 
 <img src={project2} alt="Geary Art Crawl" />
 
@@ -47,13 +64,23 @@
 </a>
 
 <p>
-  “Proforma Corps strives to engage site-specific conversations, reframe the
-  dialogue on these issues, and amplify solidarity, visibility, and legitimacy
-  with the most vulnerable affected community members. Ultimately, these actions
-  strive to bring about meaningful and impactful policy changes, as well as
-  catalyzing societal attitude transformations surrounding the essential human
-  right to housing and carceral state abolition.“
+  <i>
+    proforma corps strives to use media-architecture installations to engage in site-specific conversations, reframe dialogue on neighborhood issues, and amplify solidarity, visibility, and legitimacy with the most vulnerable affected community members. These actions aim to bring about meaningful discourse on policy, design, and collective visioning capacity, as well as catalyzing attitudinal transformations surrounding social and community needs in the built environment.
+</i>
 </p>
+
+<p>
+  proforma corps, led by Bria Cole, is an open-format artist collective that creates and collaborates to organize guerrilla urban projection interventions in Tkaronto. Using visualization, media systems, and projection media, the collective aims to collectivize issues that benefit from public dialogue, fostering community engagement and awareness.
+</p>
+
+<p>Past collective members include Mark Francis, Blythe Haynes, and Sarah Keokkeok
+</p>
+
+<p>
+  <a href="https://www.instagram.com/proforma.corps/">Learn more</a>
+</p>
+
+<hr>
 
 <img src={project3} alt="The Hastings Folk Garden" />
 
@@ -64,6 +91,7 @@
 </p>
 
 <p>
+  <i>
   “On the 100 block of East Hastings in what is colonially known as Vancouver,
   British Columbia, and which has many other names, including K’emk’em’elay, the
   Place Where the Maples Grow, the unceded lands and shared territories of the
@@ -80,7 +108,33 @@
   helps in the struggle? What holds us together? What pulls us apart? What can
   we accomplish in a season? In a year? In a lifetime? And, how might we
   envision a world beyond capitalist exploitation? Beyond policing?”
+</i>
 </p>
+
+<p>
+  <i>
+    Visit our Sound Map at: <a href="https://hivesforhumanity.com/communityengagement">hivesforhumanity.com/communityengagement</a> and listen to stories from Horace “the Bear Whisperer” Daychief, Street Mom DJ Joe, Jim McLeod, Phin, Theresa Delores Gray, Cait and Sarah
+  </i>
+</p>
+
+<p>
+  Hives for Humanity (H4H) is a non-profit organization founded in the Downtown Eastside of Vancouver, British Columbia, in 2012.  Through inclusive and supportive programming, H4H creates flexible opportunities for people to engage in the therapeutic culture that surrounds the hive. We foster self-worth and community pride through cultural and ecological skills-sharing and experiential learning, working with socially and economically vulnerable populations facing barriers to stability.
+</p>
+
+<p>
+  Through inclusive and supportive programming, H4H creates flexible opportunities for people to engage in
+the therapeutic culture that surrounds the hive. We foster self-worth and community pride through cultural
+and ecological skills-sharing and experiential learning, working with socially and economically vulnerable
+populations facing barriers to stability.
+</p>
+
+<p>Learn more at:
+<a href="https://www.hivesforhumanity.com/">
+https://www.hivesforhumanity.com/ 
+</a>
+</p>
+
+<hr>
 
 <img src={project4} alt="Ground Truth" />
 
@@ -89,6 +143,7 @@
 </p>
 
 <p>
+  <i>
   “countermap.land was the conceptual jumping off point for this project. We
   worked with artists Bruno Canadien, Sachin Sudra and Tsuut'ina Knowledge
   Keeper Kendall Jacob to share the idea of mapping and experiencing spaces
@@ -102,7 +157,33 @@
   also asked the youth to map the space via their own sensory experiences. The
   staff was tasked to update countermap.land as the Elders and Knoweldge Keepers
   preferred to deliver programs on site, without a technology component.”
+</i>
 </p>
+
+<p>
+  Stride Gallery is an artist-run centre located on the traditional territories of Tsuut’ina, Niitsitapi,
+and Iyarhe Nakoda in Treaty 7, in a city colonially known as Calgary. Tsuut’ina Nation Youth Program (TNYP) operates from Tsuut’tina Nation, facilitating the Nation’s junior powwow and various youth programs. Since 2018, our organizations have been collaborating on a youth art program named Gumisistiy, which means “to persevere” or “to keep going” in Tsuut’ina.
+</p>
+
+<p>
+  Gumisistiy Youth Program tackles the question of access to art and reciprocal relationship
+building between First Nations and art institutions. Our two organizations are building a lasting
+relationship that fosters artistic growth and agency for the youth. Acknowledging the historically
+racist and unsafe arts institutions, Gumisistiy connects Tsuut’ina youth and different art spaces
+in the city through various art workshops. Artists lead programs to introduce their own practices
+that are constantly expanding and reshaping what it means to be an Indigenous artist in this
+moment. These artists offer potential role models and pathways to finding a creative voice and
+self-expression for the youth. The program introduces ceremony to the colonial spaces, with the
+intention of making Tsuut’ina ways of being porously present in Treaty 7 areas.
+
+</p>
+
+<p>
+  Learn more at: <a href="https://www.stride.ab.ca/">https://www.stride.ab.ca/</a> and <a href="https://www.instagram.com/tsuutina.youth.program">https://www.instagram.com/tsuutina.youth.program</a> 
+
+</p>
+
+<hr>
 
 <img src={project5} alt="Hand Drawn Map" />
 
@@ -111,6 +192,7 @@
 </p>
 
 <p>
+  <i>
   “Frustrated by the absence of applied radical approaches to spatial practices
   in the city of Tkaronto and beyond, this group formed to imagine and actualize
   the potentialities of a liberated city. Our mission is to transcend the
@@ -120,7 +202,17 @@
   community partnerships. We envision a future where the city becomes a site of
   fulfillment by providing equitably for the needs and desires of its
   inhabitants.“
+</i>
 </p>
+
+<p>
+  (Re)Imagining the City Collective (RIC) is a collective of researchers, artists, cartographers, and writers concerned with space-making, counter-narratives, and agency in the city. Immersed in experimental, arts-based, and cartographic practices, RIC Collective seeks to dislocate power from authoritative planning bodies and relocate it within communities by disrupting the dominant narratives of city-building.
+</p>
+
+<p>Learn more at: <a href="https://www.instagram.com/ric.collective">https://www.instagram.com/ric.collective
+</a></p>
+
+<hr>
 
 <img src={project6} alt="Sticky notes on a wall" />
 
@@ -140,6 +232,15 @@
   or walk a are distance.”
 </p>
 
+<p>Storefront Manitoba is dedicated to advancing the awareness and appreciation of architecture, design, and urbanism. 
+</p>
+
+<p>Learn more at: 
+  <a href="https://www.storefrontmb.ca">www.storefrontmb.ca</a>
+</p>
+
+<hr>
+
 <p>
   <strong> Aiya哎呀 collective </strong>
 </p>
@@ -153,30 +254,45 @@
   the land asks of them.”
 </p>
 
+<p>aiya哎呀 is an intergenerational group of artists, chinatown community members, and friends who are addressing the ongoing displacement and cultural erasure in Edmonton’s chinatown. aiya哎呀’s work remembers and marks chinatown’s geographic and emotional losses caused by development priorities. Within these losses, aiya哎呀 creates spaces of remembering, and forms openings to a chinatown they want to see today. Guided by the frameworks of active listening and accompliceship, aiya哎呀 works towards creating alternative futures that embody the liberated world we want to see. 
+</p>
+
+<p>
+  Learn more at: 
+  <a href="https://www.instagram.com/aiyacollective/">https://www.instagram.com/aiyacollective/</a>
+</p>
+
+<hr>
+
 <p>
   <strong> The Architecture Lobby Odawa Chapter and Gallery 101 </strong>
 </p>
 
 <p>
   <i>
-    A metaphysical skin of sites where protest, resistance, and strikes took
-    place; contesting the use of sacred language and sacred sites to obscure
-    acts of oppression and exploitation;
-    <br />
-    using care and thinking about safety when sharing information about ancestral
-    sites; contest performative inclusion.
-    <br />
-    Highlight the human stories of where human beings were lost. Reconciliation quest
-    For all relations
-    <br />
-
-    sites of repression shut down sites of resistance.
+    Gallery 101 exhibits contemporary visual, media, and performance arts that explore self-representation of decolonizing, feminist, intersectional counter-narratives. We strive to be an inclusive contemporary art space with awareness of current social, cultural, environmental discourses and movements guiding our curatorial practices. Powerful programming partnerships support our mission to present projects that provide a range of entry points for audiences to engage with and reimagine issues especially related to land, water, race, histories, gender, ability, age and the relationships between Indigenous, settler, newcomer, and migrant communities. 
   </i>
 </p>
 
+<p>
+  TAL-Ottawa(Odawa)/TAL-OT is a collective of workers, students and faculty in architecture and geography (autonomous from academic institutions) who aim to establish a presence of advocacy in architecture and companion professions. 
+</p>
+
+<p>
+  Learn more at: <a href="https://g101.ca/">https://g101.ca/</a> 
+</p>
 
 <style>
-    p {
+    p, a, img, hr {
         margin-bottom: 1.5rem;
+        font-size: 0.875rem;
+    }
+
+    a {
+      text-decoration: underline;
+    }
+
+    hr {
+      border: 1px solid #000;
     }
 </style>
