83accc8404e573463f46ca5c88607a9e289814a5
vincent <vincentcharlebois@gmail.com>
2024-04-02 21:22:36 -0400

    Turn Logo to HTML to bring back text shadows

--- src/lib/UI/Logo.svelte
+++ src/lib/UI/Logo.svelte
@@ -1,13 +1,11 @@
 <script>
   import { onMount } from "svelte";
 
-  import svgLogo from "$lib/icons/logo.svg";
   import { base } from "$app/paths";
   import { darkMode } from "$lib/stores";
 
   
   export let showOnMobile = false;
-  let iconHtml;
   let isMobileScreen = false; 
 
   onMount(() => {
@@ -15,25 +13,33 @@
     isMobileScreen = window.innerWidth <= 768;
   });
 
-  $: iconHtml =
-    $darkMode === "dark"
-      ? `<div style="width: 16rem; height: auto; filter: invert(100%);">
-          <img src="${svgLogo}" alt="*countermap" style="width: 100%; height: 100%;" />
-        </div>`
-      : `<div style="width: 16rem; height: auto;">
-          <img src="${svgLogo}" alt="Monument Marker" style="width: 100%; height: 100%;" />
-        </div>`;
 </script>
 
-<h1 style="display: {isMobileScreen && !showOnMobile ? 'none' : 'block'};">
+<h1 class={$darkMode === "dark" ? "darkmode" : ""} style="display: {isMobileScreen && !showOnMobile ? 'none' : 'block'};">
   <a href="{base}/">
-    {@html iconHtml}
+    <span>*</span>
+    countermap
   </a>
 </h1>
 
 <style>
+   .darkmode {
+    color: white;
+    text-shadow: 1px 1px 1px rgb(0, 0, 0);
+  }
   h1 {
     margin-top: 1rem;
     z-index: 10001 !important;
+    font-family: "BCSans";
+    font-weight: 600;
+    font-size: 2.5rem;
+  }
+  span {
+    position:absolute;
+    font-family: "Itim";
+    margin-top:-1.5rem;
+    margin-left:-1.35rem;
+    font-size: 4rem;
+    font-weight: 400;
   }
 </style>
